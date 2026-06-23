bc16aeaa3bdbaf99659dbff1c0a1da7b1650786b
abinaya <abinayasonagnanasekaran@gmail.com>
2026-06-23 12:12:01 +0530

    new changes

--- src/Headers1.js
+++ src/Headers1.js
@@ -5,15 +5,15 @@ import { FaSearch } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 const Headers1=()=>{
-    const [activeItem,setactiveItem]=useState("");
+    
     const listanchor=[
         {
             id:1,
             name:"Home",
             lined:"false",
-            path:" "
+            path:"/"
         },
         {
             id:2,
@@ -25,13 +25,13 @@ const Headers1=()=>{
             id:3,
             name:"About us",
             lined:"false",
-            path:"About"
+            path:"/About"
         },
         {
             id:4,
             name:"Contact",
             lined:"false",
-            path:"contact"
+            path:"/contact"
         }
     ];
     return(
@@ -44,12 +44,9 @@ const Headers1=()=>{
             {listanchor.map( (list)=>(
                 <div>
                     <ul>
-                        <li><Link to="./ {list.path}"
-                        className={activeItem===list.name?"underline-active":""}
-                        onClick={(e)=>{
-                            e.preventDefault();
-                            setactiveItem(list.name);
-                        }}>{list.name}</Link></li>
+                        <li><NavLink to={list.path}
+                        className={({isActive})=>isActive?"underline-active":""}
+                        >{list.name}</NavLink></li>
                     </ul>
                 </div>
              ) )}
@@ -65,7 +62,7 @@ const Headers1=()=>{
                 <a href="./"><FaRegHeart /></a>
             </li>
             <li>
-                <Link to="./Cart"><FaShoppingCart /></Link>
+                {/* <Link to="/Cart"><FaShoppingCart /></Link> */}
             </li>
            </ul>
            
